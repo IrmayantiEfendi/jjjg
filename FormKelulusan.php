dec03aaf400a539ba0ae7fd01dbbd54088daf831
IrmayantiEfendi <adeirmayantiefendy@gmail.com>
2017-07-14 08:59:05 +0700

    upload

---
+++ FormKelulusan.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kelulusan</title>
+</head>
+<body>
+
+	<form method="get">
+		<input type="text" name="Nomor" placeholder="Silakan Isi Nomor Anda"> <label>Nomor</label>
+	</form>
+
+</body>
+</html>
